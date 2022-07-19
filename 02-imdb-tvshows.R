de844c48a17567e38b4255eaa81e163e8ff102c3
thomasbsease <thomas.b.sease@icloud.com>
2022-07-19 15:44:42 -0500

    final

--- 02-imdb-tvshows.R
+++ 02-imdb-tvshows.R
@@ -7,32 +7,40 @@ library(rvest)
 
 # read in http://www.imdb.com/chart/tvmeter ------------------------------------
 
-page <- read_html("___")
+page <- read_html("https://www.imdb.com/chart/tvmeter")
 
 # years ------------------------------------------------------------------------
 
 years <- page %>%
-  html_nodes("___") %>%
-  html_text() %>%
-  ___
+  html_nodes("a+ .secondaryInfo") %>%
+  html_text() %>% 
+  str_remove("\\(") %>% 
+  str_remove("\\)") %>% 
+  as.numeric()
 
 # scores -----------------------------------------------------------------------
 
 scores <- page %>%
-  ___
+  html_nodes(".imdbRating") %>% 
+  html_text() %>% 
+  as.numeric()
 
 # names ------------------------------------------------------------------------
 
-names <- ___
+names <- page %>% 
+  html_nodes(".titleColumn a") %>% 
+  html_text() %>% 
+  str_remove_all("\n") %>% 
+  str_squish()
+  
 
 # tvshows dataframe ------------------------------------------------------------
 
 tvshows <- tibble(
   rank = 1:100,
-  ___,
-  ___,
-  ___,
-  ___
+  scores = scores,
+  names = names,
+  years = years
 )
 
 # add new variables ------------------------------------------------------------
@@ -46,18 +54,19 @@ tvshows <- tvshows %>%
 
 # add new info for first show --------------------------------------------------
 
-tvshows$genre[1] <- "__"
-tvshows$runtime[1] <- ___
-tvshows$n_episode[1] <- ___
+tvshows$genre[1] <- "Horror"
+tvshows$runtime[1] <- "300"
+tvshows$n_episode[1] <- "12"
 
 # add new info for second show --------------------------------------------------
 
-tvshows$genre[2] <- "__"
-tvshows$runtime[2] <- ___
-tvshows$n_episode[2] <- ___
+tvshows$genre[2] <- "Action"
+tvshows$runtime[2] <- "320"
+tvshows$n_episode[2] <- "5"
 
 # add new info for third show --------------------------------------------------
 
-tvshows$genre[3] <- "__"
-tvshows$runtime[3] <- ___
-tvshows$n_episode[3] <- ___
+tvshows$genre[3] <- "Fantasy"
+tvshows$runtime[3] <- "500"
+tvshows$n_episode[3] <- "15"
+
